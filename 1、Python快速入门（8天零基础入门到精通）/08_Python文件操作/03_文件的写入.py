1a9b9f85113eff972f1332c5b4ab3aeec0e18fc5
Chenjun <@503>
2025-04-01 17:59:21 +0800

    快速入门-08_Python文件操作

--- 1、Python快速入门（8天零基础入门到精通）/08_Python文件操作/03_文件的写入.py
+++ 1、Python快速入门（8天零基础入门到精通）/08_Python文件操作/03_文件的写入.py
@@ -5,3 +5,17 @@
     2. 当调用flush时，文件才会真正写入文件中
     3. 这是为了避免频繁操作硬盘，导致效率下降
 """
+
+file_path = "C:/Develop/PythonProjects/MyPythonLearning/1、Python快速入门（8天零基础入门到精通）/08_Python文件操作/test/写入.txt"
+# 打开文件
+file = open(file_path, "w", encoding='utf-8')
+
+# 文件写入
+file.write("你好Python，再见Python")
+
+# 内容刷新
+file.flush()
+
+# 关闭文件
+file.close()
+
---
+++ 1、Python快速入门（8天零基础入门到精通）/08_Python文件操作/04_文件的追加.py
@@ -0,0 +1,18 @@
+"""
+文件追加（继续在文件原始内容后续写）
+注意：
+    1. 追加模式中，目标文件必须已存在，写入内容会在目标文件的最后继续写入
+"""
+
+file_path = "C:/Develop/PythonProjects/MyPythonLearning/1、Python快速入门（8天零基础入门到精通）/08_Python文件操作/test/追加.txt"
+# 读取文件
+file = open(file_path, 'a', encoding='utf-8')
+
+# 写入内容
+file.write("你好Python，再见Python")
+
+# 刷新内容
+file.flush()
+
+# 关闭文件
+file.close()
